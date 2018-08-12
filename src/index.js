5c3e04a66c69d33614f16686f2becfead2b62a11
Panayotis Matsinopoulos <>
2018-08-12 13:10:42 +0300

    My simple Vue demo using webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue/dist/vue.esm';
+
+let vm = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello World 2'
+  }
+});
+
+export default vm;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
 };
